c3cf6c98bc5c971e8db522dfe49ce304b56fcd73
E <emirhansergen@gmail.com>
2025-07-09 16:26:08 +0300

    Etiya , Project-X entry

---
+++ Notification/src/test/java/com/projectx/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectx.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
